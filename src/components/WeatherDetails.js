aeb5eed59104db6928ab0955c97f89aa54928950
Gupta_ Rajeshwari (rajeshwgupta@deloitte.com) <rajeshwgupta@your.hostname.com>
2024-01-10 18:17:13 +0530

    sunrise sunset timing

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,28 +1,77 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./WeatherDetails.css";
 import axios from "axios";
 
 const WeatherDetails = () => {
   const [data, setData] = useState("");
   const { name } = useParams();
+  const [sunrise, setSunrise] = useState(null);
+  const [sunset, setSunset] = useState(null);
+  const [dayLength, setDayLength] = useState(null);
+  const [remainingDaylight, setRemainingDaylight] = useState(null);
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=eb5fd6ce795d040ed78f3f6225960fa2`;
   useEffect(() => {
     axios.get(url).then((response) => {
       setData(response.data);
     });
   }, []);
+  console.log(data);
   const currentTime = new Date().toLocaleTimeString("en-US", {
     hour: "numeric",
     minute: "numeric",
     hour12: true,
   });
-//   console.log(currentTime);
+  const convertUnixTimestampToTime = (unixTimestamp) => {
+    const date = new Date(unixTimestamp * 1000);
+    const hours = date.getHours();
+    const minutes = "0" + date.getMinutes();
+    return hours + ':' + minutes.substr(-2);
+  };
+
+  const calculateDayLength = (sunrise, sunset) => {
+    const sunriseTime = convertUnixTimestampToTime(sunrise);
+    const sunsetTime = convertUnixTimestampToTime(sunset);
+
+    const sunriseDate = new Date(sunrise * 1000);
+    const sunsetDate = new Date(sunset * 1000);
+
+    
+    const dayLengthInMilliseconds = sunsetDate - sunriseDate;
+    const hours = Math.floor(dayLengthInMilliseconds / 3600000);
+    const minutes = Math.floor((dayLengthInMilliseconds % 3600000) / 60000);
+    const dayLength = `${hours}H ${minutes}M`;
+
+    
+    const remainingDaylightInMilliseconds = sunsetDate - new Date();
+    const remainingHours = Math.floor(remainingDaylightInMilliseconds / 3600000);
+    const remainingMinutes = Math.floor((remainingDaylightInMilliseconds % 3600000) / 60000);
+    const remainingDaylight = `${remainingHours}h ${remainingMinutes}m`;
+
+    setSunrise(sunriseTime);
+    setSunset(sunsetTime);
+    setDayLength(dayLength);
+    setRemainingDaylight(remainingDaylight);
+  };
+  useEffect(() => {
+    
+    const sunriseValue = data?.sys?.sunrise;
+    const sunsetValue = data?.sys?.sunset;
+    calculateDayLength(sunriseValue, sunsetValue);
+  }, []);
+  
+ 
 
   return (
     <>
       <div className="weatherContainer">
         <div className="breadcrump">
+        <Link to={"/"}>
+            <div className="back">
+              <img src="../asset/arrow_back_ios_24px.svg" alt="left" />
+              <div>Back</div>
+            </div>
+          </Link>
           <div className="bt">
             <button className="red">Remove</button>
           </div>
@@ -42,7 +91,7 @@ const WeatherDetails = () => {
 
         <div className="Row-2">
           <div className="c-1">
-            TIMEs
+            TIME
             <p>{currentTime}</p>
           </div>
           <div className="c-1">
@@ -64,10 +113,10 @@ const WeatherDetails = () => {
           <div className="rr">
             <div className="Time">
               <div>
-                Length of day : <span className="time">13H 12M</span>
+                Length of day : <span className="time">{dayLength}</span>
               </div>
               <div>
-                Remaining daylight : <span className="time">9H 22M</span>
+                Remaining daylight : <span className="time">{remainingDaylight}</span>
               </div>
             </div>
             <div>GRAPH</div>
