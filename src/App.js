1f7697669593e6188af3aa4a91eea05e75b72dbf
Gupta_ Rajeshwari (rajeshwgupta@deloitte.com) <rajeshwgupta@your.hostname.com>
2024-01-10 16:17:30 +0530

    Weather page with basic details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar";
 import "./App.css";
 import DashBoard from "./components/DashBoard";
+import WeatherDetails from "./components/WeatherDetails";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<DashBoard />}></Route>
+        <Route path="/weather/:name" element={<WeatherDetails/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,31 +1,31 @@
 import "./Card.css";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Card = ({ data }) => {
-//   console.log(data.weather[0].description);
-
   return (
     <>
-      <div className="Card">
-        <div className="row1">
-          <div>{data.name}</div>
-          <img src="./asset/Right.svg" alt="right-arrow"></img>
-        </div>
-        <div className="row2">
-          <div className="r2">
-            <h3>{(data?.main?.temp - 273.15).toFixed(2)}</h3>
-            <img src="/asset/Ellipse 1.svg " className="deg" alt="degree" />
+      <Link to={`/weather/${data.name}`}>
+        <div className="Card">
+          <div className="row1">
+            <div>{data.name}</div>
+            <img src="./asset/Right.svg" alt="right-arrow"></img>
           </div>
-          <img src="" alt="cloud"></img>
-        </div>
-        <div className="row3">
-          <div className="col1">
-            <img src="./asset/warning.svg " alt="sign" />
-            <p className="col2">{data?.weather[0]?.description}</p>
+          <div className="row2">
+            <div className="r2">
+              <h3>{(data?.main?.temp - 273.15).toFixed(1)}</h3>
+              <img src="./asset/Ellipse 1.svg " className="deg" alt="degree" />
+            </div>
+            <img src={`http://openweathermap.org/img/w/${data?.weather[0]?.icon}.png`} alt="cloud" className="weather_icon_api"></img>
           </div>
-          <div className="col2">{data?.weather[0]?.main}</div>
+          <div className="row3">
+            <div className="col1">
+              <img src="./asset/warning.svg " alt="sign" />
+              <p className="col2">{data?.weather[0]?.description}</p>
+            </div>
+            <div className="col2">{data?.weather[0]?.main}</div>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
